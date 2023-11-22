a3a07983db91aee0819ca7cf0f12982f1ae71c92
GustavoBonif <gubonic@gmail.com>
2023-11-22 09:29:27 -0300

    setup: creating eureka-server service

---
+++ src/main/java/com/diningroom/eureka/DiningRoomEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.diningroom.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiningRoomEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiningRoomEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diningroom/eureka/DiningRoomEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diningroom.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiningRoomEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
